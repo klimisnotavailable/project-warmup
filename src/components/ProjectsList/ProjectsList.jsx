7680fd831883850c55c99ee0dbf86549b49db642
klimisnotavailable <klimgrigor28@gmail.com>
2024-07-30 14:53:20 +0300

    projects

---
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -0,0 +1,40 @@
+const ProjectsList = () => {
+  return (
+    <ul className="grid grid-cols-2 gap-4 p-5 ">
+      <li className=" bg-white rounded-xl p-10 text-left">
+        <div className="w-2/3">
+          <h3 className="uppercase semi-bold ">Hospital Managment System</h3>
+          <a className="mb-4" href="" >Subheading</a>
+          <p className="text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, asperiores!</p>
+        </div>
+        <img src="" alt="" />
+      </li>
+      <li className="bg-white w-auto rounded-xl p-10 text-left">
+        <div className="w-2/3">
+          <h3 className="uppercase semi-bold ">Hospital Managment System</h3>
+          <a className="mb-4" href="" >Subheading</a>
+          <p className="text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, asperiores!</p>
+        </div>
+        <img src="" alt="" />
+      </li>
+      <li className="bg-white w-auto rounded-xl p-10 text-left">
+        <div className="w-2/3">
+          <h3 className="uppercase semi-bold ">Hospital Managment System</h3>
+          <a className="mb-4" href="" >Subheading</a>
+          <p className="text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, asperiores!</p>
+        </div>
+        <img src="" alt="" />
+      </li>
+      <li className="bg-white w-auto rounded-xl p-10 text-left">
+        <div className="w-2/3">
+          <h3 className="uppercase semi-bold ">Hospital Managment System</h3>
+          <a className="mb-4" href="" >Subheading</a>
+          <p className="text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, asperiores!</p>
+        </div>
+        <img src="" alt="" />
+      </li>
+    </ul>
+  );
+};
+
+export default ProjectsList;
--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -1,8 +1,11 @@
+import ProjectsList from "../ProjectsList/ProjectsList";
+
 const ProjectsSection = () => {
     return (
         <section className="p-24">
             <div className="bg-indigo-800 p-10 text-center">
                 <h2 className="text-white text-2xl">Projects</h2>
+                <ProjectsList></ProjectsList>
             </div>
         </section>
     )
